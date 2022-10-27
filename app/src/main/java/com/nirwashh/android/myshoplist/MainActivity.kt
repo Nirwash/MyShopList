787be8deab1fb52432b7885c5c95fddda306022b
Rubanov Andrey <nirwash@bk.ru>
2022-10-27 06:55:27 +0300

    initial

---
+++ app/src/main/java/com/nirwashh/android/myshoplist/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nirwashh.android.myshoplist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nirwashh.android.myshoplist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
