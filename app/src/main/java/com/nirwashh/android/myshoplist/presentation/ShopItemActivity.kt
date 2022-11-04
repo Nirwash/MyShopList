347b8a4ee37b49b65630144819849d1bea804734
Rubanov Andrey <nirwash@bk.ru>
2022-11-04 23:16:26 +0300

    add intent and mode for start activity

--- app/src/main/java/com/nirwashh/android/myshoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/nirwashh/android/myshoplist/presentation/ShopItemActivity.kt
@@ -1,8 +1,10 @@
 package com.nirwashh.android.myshoplist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.nirwashh.android.myshoplist.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.nirwashh.android.myshoplist.databinding.ActivityShopItemBinding
 
 class ShopItemActivity : AppCompatActivity() {
@@ -11,5 +13,26 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityShopItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
