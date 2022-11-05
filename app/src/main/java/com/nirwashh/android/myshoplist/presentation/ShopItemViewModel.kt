9304778819f8cba8bf886f87b12f51a04033bead
Rubanov Andrey <nirwash@bk.ru>
2022-11-05 07:18:08 +0300

    finish ShopItemActivity.kt

--- app/src/main/java/com/nirwashh/android/myshoplist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/nirwashh/android/myshoplist/presentation/ShopItemViewModel.kt
@@ -45,7 +45,7 @@ class ShopItemViewModel : ViewModel() {
         }
     }
 
-    fun editShopItem(shopItemId: Int, inputName: String?, inputCount: String?) {
+    fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
